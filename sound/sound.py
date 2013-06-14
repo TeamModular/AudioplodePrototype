dcbf5664fd0befbd409d103813961dec604679f1
Richard Lowe <b0ggyb33@gmail.com>
2013-06-14 13:13:14 +0100

    Made a better interface for sound

--- sound/sound.py
+++ sound/sound.py
@@ -2,10 +2,16 @@ import time
 import numpy
 import pyaudio
 import fluidsynth
+import thread
 
 class sound:
 	def __init__(self):
-
+		"""
+		Creates the handles and initalises
+		fluidsynth soundbank
+		calls _genTunes to generate the sounds
+		"""
+		
 		pa = pyaudio.PyAudio()
 		self._strm = pa.open(format = pyaudio.paInt16,channels = 2, rate = 44100,  output = True)
 		
@@ -13,48 +19,43 @@ class sound:
 		# Initial silence is 1 second
 		sfid = self._fl.sfload("/usr/share/sounds/sf2/FluidR3_GM.sf2")
 		self._fl.program_select(0, sfid, 0, 0)
-	
-		self.gen()
-		self.genHigh()
-		self.genOct()
-		##fl.delete()
+		
+		self._genTunes()
+				
+		self._handles=[pa.open(format = pyaudio.paInt16,channels = 2, rate = 44100,  output = True) for handles in xrange(8)] #generate 8 handles to use
 
-	def gen(self):
+	def _genTunes(self):
+		"""
+		Fudging audio creation for now by just having 
+		sounds made that all belong to
+		one private list
+		"""
+		self._tunes=[]
+		
 		s=[]
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
-
 		self._fl.noteon(0, 60, 120)
-		#self._fl.noteon(0, 67, 120)
-		#self._fl.noteon(0, 76, 120)
-		
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.3)))
 		self._fl.noteoff(0, 60)
-		#self._fl.noteoff(0, 67)
-		#self._fl.noteoff(0, 76)
-	
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
+		self._tunes.append(fluidsynth.raw_audio_string(s))
 
-		self._samps = fluidsynth.raw_audio_string(s)
-		
-
-	def genHigh(self):
 		s=[]
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
-
-		#self._fl.noteon(0, 60, 120)
-		#self._fl.noteon(0, 67, 120)
-		self._fl.noteon(0, 76, 120)
-		
+		self._fl.noteon(0, 67, 120)
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.3)))
-		#self._fl.noteoff(0, 55)
-		#self._fl.noteoff(0, 67)
-		self._fl.noteoff(0, 76)
-		
+		self._fl.noteoff(0, 67)
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
+		self._tunes.append(fluidsynth.raw_audio_string(s))
 
-		self._sampsHigh = fluidsynth.raw_audio_string(s)
-	
-	def genOct(self):
+		s=[]
+		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
+		self._fl.noteon(0, 76, 120)
+		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.3)))
+		self._fl.noteoff(0, 76)
+		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
+		self._tunes.append(fluidsynth.raw_audio_string(s))
+
 		s=[]
 		s = numpy.append(s, self._fl.get_samples(int(44100 * 0.1)))
 		for i in xrange(0,32,4):
@@ -65,14 +66,20 @@ class sound:
 			self._fl.noteon(0, 60+i, 120)
 			s = numpy.append(s, self._fl.get_samples(int(44100 * 0.2)))
 			self._fl.noteoff(0, i)
-			
-		self._sampsOct = fluidsynth.raw_audio_string(s)	
+		self._tunes.append(fluidsynth.raw_audio_string(s))
+		
+		return
+		
+	def play(self,index):
+		"""
+		Given an integer index
+		will play the corresponding sound
+		"""
+		assert type(index)==int
+		assert index<len(self._tunes), "Trying to access unavailable tune. Make more Tunez"
+		stream=self._handles.pop(0)	#pop handle from front
+
+		thread.start_new_thread(stream.write,(self._tunes[index],) )
 	
-	def base(self):
-		self._strm.write(self._samps)
-		
-	def high(self,stream):
-		stream.write(self._sampsHigh)
-		
-	def octave(self,stream):
-		stream.write(self._sampsOct)
+		self._handles.append(stream) #push at end
+	
--- sound/test.py
+++ sound/test.py
@@ -1,23 +1,19 @@
 import sound
-import thread
+#import thread
 import time
 
 test=sound.sound()
 
-import fluidsynth
-import numpy
-import pyaudio
-pa = pyaudio.PyAudio()
-strm = pa.open(format = pyaudio.paInt16,channels = 2, rate = 44100,  output = True)
-
-handles=[pa.open(format = pyaudio.paInt16,channels = 2, rate = 44100,  output = True) for i in xrange(29)]
-
-for i in xrange(29):
-	thread.start_new_thread(test.octave,(handles[i],))			
-	time.sleep(1)
-
-#test.base()
-#test.high()
-#test.octave()
 print 'done'
-time.sleep(5)
+
+test.play(0)
+test.play(1)
+test.play(2)
+#test.play(3)
+
+time.sleep(0.4)
+
+test.play(3)
+
+time.sleep(3.5)
+
