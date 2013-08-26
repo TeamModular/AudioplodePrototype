83f0dd0e407e8ddf7fe78e277e9b54602752d363
Michael Hodgson <michaelwhodgson@gmail.com>
2013-08-26 19:04:43 +0100

    Added a tower sprite and some terrible rendering code.
    - Rendering code does not work properly. Rotation is all messed up. When
      the turret rotates it's centre point moves. Need to figure out how to
      fix this. Probably simple.

--- src/Tower.py
+++ src/Tower.py
@@ -6,7 +6,10 @@ Created on 12 Jun 2013
 
 from Cell import Cell
 import pygame
-from numpy import array as Vector,linalg
+from numpy import array as Vector, linalg
+
+omniTowerBaseSprite = pygame.image.load("../art/omni-tower/omni-tower-base.png")
+omniTowerTopSprite = pygame.image.load("../art/omni-tower/omni-tower-gun-top.png")
 
 class Tower(Cell):
     '''
@@ -19,7 +22,7 @@ class Tower(Cell):
         '''
         Constructor
         '''
-        
+        self.rotation = 0
         self.range=6
         self.range2 = self.range*self.range
         #damage per shot
@@ -32,10 +35,10 @@ class Tower(Cell):
         #self.pos = Vector(x,y)
         
     def draw(self, screen, x, y, size):
-        self.drawStatic(screen, x, y, size)
+        self.drawStatic(screen, x, y, size, rotation=self.rotation)
         
     @staticmethod
-    def drawStatic(screen,x,y,size):
+    def drawStatic(screen,x,y,size,rotation=0):
         '''
         TODO review if htis is the best way of doing this
         
@@ -43,7 +46,27 @@ class Tower(Cell):
         
         when/if specific stuff is required to be drawn, use argumetns with defaults?
         '''
-        pygame.draw.rect(screen, (0,0,255), pygame.Rect(x,y,size,size), 0)
+        # calculate x scale and y scale change
+        xScale = float(size) / float(omniTowerTopSprite.get_width())
+        yScale = float(size) / float(omniTowerTopSprite.get_height())
+        
+        topRotated = pygame.transform.rotate(omniTowerTopSprite, rotation)
+        topResized = pygame.transform.smoothscale(topRotated, (int(float(topRotated.get_width()) * xScale), int(float(topRotated.get_height()) * yScale)))
+        
+        baseResized = pygame.transform.smoothscale(omniTowerBaseSprite, (size, size))
+        
+        #widthDiff = topResized.get_width() - baseResized.get_width()
+        #heightDiff = topResized.get_height() - baseResized.get_height()
+        
+        # find centre of tower
+        xCentre = float(x) + (float(size) / 2.0)
+        yCentre = float(y) + (float(size) / 2.0)
+        
+        screen.blit(baseResized, (x, y))
+        
+        screen.blit(topResized, (round(xCentre - float(topResized.get_width()) / 2.0), round(yCentre - float(topResized.get_height()) / 2.0)))
+        
+        #pygame.draw.rect(screen, (0,0,255), pygame.Rect(x,y,size,size), 0)
     
     def towerable(self):
         return False
@@ -51,7 +74,7 @@ class Tower(Cell):
     def update(self, dt, mobs):
         
         self.temperature=self.temperature-dt
-        
+        self.rotation += dt * 40
         if self.temperature<=0:
         #only actually check for mobs to shoot at if we can shoot
             for mob in mobs:
