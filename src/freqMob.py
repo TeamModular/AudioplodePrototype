1c728e8aa39808d5b4fc31e6cc1de05da4c408af
b0ggyb33 <b0ggyb33@gmail.com>
2013-10-01 23:24:17 +0100

    Modifid freqMob to change the colour of mobs as they take damage, a precursor to freeze towers

--- src/freqMob.py
+++ src/freqMob.py
@@ -1,7 +1,20 @@
-import mob
+import mob as mobClass
 
-class freqMob(mob.mob):
+class mob(mobClass.mob):
     def __init__(self,position,path):
-        super(freqMob,self).__init__(position,path)
+        mobClass.mob.__init__(self,position,path)
+        #super(mob,self).__init__(position,path)
+        self._frequency = 1
+    
+    def scale(self,multiple):
+        self._frequency=multiple
         
+    def update(self,dt,newPath=None):
+        mobClass.mob.update(self,dt,newPath=None)
+        self._colour = (255*self._frequency,0,255*self._frequency)
         
+     
+    def damage(self,amount):
+        mobClass.mob.damage(self,amount)
+        frac=self._health/100. #get a fraction of total life left
+        self.scale(frac)
