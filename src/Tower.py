e562fe3c712ea26f62088c0a9ab28234627d77c1
Michael Hodgson <michaelwhodgson@gmail.com>
2013-10-09 19:28:12 +0100

    Revert "Added a tower sprite and some terrible rendering code."
    This reverts commit 83f0dd0e407e8ddf7fe78e277e9b54602752d363.

--- src/Tower.py
+++ src/Tower.py
@@ -6,10 +6,7 @@ Created on 12 Jun 2013
 
 from Cell import Cell
 import pygame
-from numpy import array as Vector, linalg
-
-omniTowerBaseSprite = pygame.image.load("../art/omni-tower/omni-tower-base.png")
-omniTowerTopSprite = pygame.image.load("../art/omni-tower/omni-tower-gun-top.png")
+from numpy import array as Vector,linalg
 
 class Tower(Cell):
     '''
@@ -22,7 +19,7 @@ class Tower(Cell):
         '''
         Constructor
         '''
-        self.rotation = 0
+        
         self.range=6
         self.range2 = self.range*self.range
         #damage per shot
@@ -35,10 +32,10 @@ class Tower(Cell):
         #self.pos = Vector(x,y)
         
     def draw(self, screen, x, y, size):
-        self.drawStatic(screen, x, y, size, rotation=self.rotation)
+        self.drawStatic(screen, x, y, size)
         
     @staticmethod
-    def drawStatic(screen,x,y,size,rotation=0):
+    def drawStatic(screen,x,y,size):
         '''
         TODO review if htis is the best way of doing this
         
@@ -46,27 +43,7 @@ class Tower(Cell):
         
         when/if specific stuff is required to be drawn, use argumetns with defaults?
         '''
-        # calculate x scale and y scale change
-        xScale = float(size) / float(omniTowerTopSprite.get_width())
-        yScale = float(size) / float(omniTowerTopSprite.get_height())
-        
-        topRotated = pygame.transform.rotate(omniTowerTopSprite, rotation)
-        topResized = pygame.transform.smoothscale(topRotated, (int(float(topRotated.get_width()) * xScale), int(float(topRotated.get_height()) * yScale)))
-        
-        baseResized = pygame.transform.smoothscale(omniTowerBaseSprite, (size, size))
-        
-        #widthDiff = topResized.get_width() - baseResized.get_width()
-        #heightDiff = topResized.get_height() - baseResized.get_height()
-        
-        # find centre of tower
-        xCentre = float(x) + (float(size) / 2.0)
-        yCentre = float(y) + (float(size) / 2.0)
-        
-        screen.blit(baseResized, (x, y))
-        
-        screen.blit(topResized, (round(xCentre - float(topResized.get_width()) / 2.0), round(yCentre - float(topResized.get_height()) / 2.0)))
-        
-        #pygame.draw.rect(screen, (0,0,255), pygame.Rect(x,y,size,size), 0)
+        pygame.draw.rect(screen, (0,0,255), pygame.Rect(x,y,size,size), 0)
     
     def towerable(self):
         return False
@@ -74,7 +51,7 @@ class Tower(Cell):
     def update(self, dt, mobs):
         
         self.temperature=self.temperature-dt
-        self.rotation += dt * 40
+        
         if self.temperature<=0:
         #only actually check for mobs to shoot at if we can shoot
             for mob in mobs:
