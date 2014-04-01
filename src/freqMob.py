c958760e2c60797b3b8c5d8a8ad6a55939af2881
b0ggyb33 <b0ggyb33@gmail.com>
2014-04-01 17:27:13 +0100

    Modified mob speed to be dependant on size

--- src/freqMob.py
+++ src/freqMob.py
@@ -5,29 +5,28 @@ class mob(mobClass.mob):
         mobClass.mob.__init__(self,position,path)
         #super(mob,self).__init__(position,path)
         self._frequency = 1
-        self._sizeControl=size
-        if size>0.1:
-            if size<1:
-                self._size=(size,size)
-                self._health=100*size
-                self._soundValue=int(10*size) #range 1..10
-            else:
-                self._size=(1.0,1.0)
-                self._health=100
-                self._soundValue=10
-        else:
-            self._size=(0.1,0.1)
-            self._health=1
-            self._soundValue=1
+        
+        self.speed
 
+        if size>0.1:
+            if size>1:
+                size=1
+        else:
+            size=0.1
+        self._sizeControl=size                
+        
+        self._size=(size,size)
+        self._health=100*size
+        self._soundValue=int(10*size) #range 1..10
+        self.speed=4*(1-size)
             
     def scale(self,multiple):
         self._frequency=multiple
-        
+                        
     def update(self,dt,newPath=None):
         mobClass.mob.update(self,dt,newPath)
-        self._colour = (255*self._frequency,0,255*self._frequency)
-             
+        self._colour =(255*self._frequency,0,255*self._frequency)                 
+        
     def damage(self,amount):
         mobClass.mob.damage(self,amount)
         frac=self._health/(100.0*self._sizeControl) #get a fraction of total life left
